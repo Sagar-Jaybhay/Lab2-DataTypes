2638947ccc1b8e1def3214e4aa9a335246d826aa
Sagar-Jaybhay <sagarjaybhay18091988@gmail.com>
2017-05-26 00:23:40 +0530

    added new labs

---
+++ Lab35-StackAndQueue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Lab35_StackAndQueue
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Stack st = new Stack();
+            for(int i=0;i<5;i++)
+            {
+                st.Push(i+1);
+            }
+
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine(st.Pop());
+            }
+        
+          
+
+           
+            Console.Read();
+        }
+    }
+}
---
+++ Lab37-SpecializedCollection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+namespace Lab37_SpecializedCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========start of CollectionUtile ===============================");
+            Hashtable tw = CollectionsUtil.CreateCaseInsensitiveHashtable();
+            tw.Add("sa","sagar");
+            tw.Add("ma", "madam");
+            Console.WriteLine(tw["SA"]);
+
+            Console.WriteLine("=========start of ListtDictionry ===============================");
+            ListDictionary ls = new ListDictionary();
+            ls.Add("sa", "sujit");
+            ls.Add("ma", "madam");
+            Console.WriteLine(ls["sa"]);
+
+            Console.WriteLine("================start of hybrid Dictionary========================");
+            HybridDictionary hs = new HybridDictionary();
+            hs.Add("sa", "prem");
+            hs.Add("ma", "madam");
+            Console.WriteLine(hs["sa"]);
+
+
+            Console.WriteLine("===============String Collection=========================");
+            StringCollection st = new StringCollection();
+            st.Add("sagar");
+            st.Add("sujit");
+            Console.WriteLine(st[0]);
+
+            Console.WriteLine("========================================");
+
+            Console.Read();
+        }
+    }
+}
