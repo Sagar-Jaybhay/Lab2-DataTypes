23eaab4946375209fc46a6d1221ac7ce90998b2e
Sagar-Jaybhay <sagarjaybhay18091988@gmail.com>
2017-05-31 23:03:42 +0530

    added new lab 40

---
+++ Lab40-PartialMethod/PartialClass1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab40_PartialMethod
+{
+    public partial class Example
+    {
+        partial void  display();
+        partial void display(int i);
+        //partial void display()
+        //{
+        //    Console.WriteLine("I am partial method");
+        //}
+        public void NormalMethod()
+        {
+            Console.WriteLine("i m in normal method");
+            display();
+            display(10);
+        }
+
+    }
+}
---
+++ Lab40-PartialMethod/partialclass2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab40_PartialMethod
+{
+    public partial class Example
+    {
+        partial void display()
+        {
+            Console.WriteLine("I am partial method second time");
+        }
+
+        partial void display(int i)
+        {
+            Console.WriteLine("Overloaded method "+i);
+
+        }
+    }
+}
